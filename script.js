17a67b892e3f5caa732ae356876eeef890b70403
Anneke Hugenberg <Annekehugenberg@googlemail.com>
2024-02-20 23:20:11 +0100

    add code for lecture about selecting, creating and deleting elements in the DOM

--- script.js
+++ script.js
@@ -31,3 +31,44 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// selecting, creating and deleting Elements
+// selecting elements
+console.log(document.documentElement); // select the entire document
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1'); // creates a node-List
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons); // returns an HTML Collection, when the DOM is changed, it will immediatly change
+
+console.log(document.getElementsByClassName('btn'));
+
+// creating and inserting elements
+
+const message = document.createElement('div'); // just an object that represents a DOM ELement
+message.classList.add('cookie-message');
+// message.textContent =
+//   'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>'; // to read or set content
+
+header.prepend(message); // adds the element as the first child to the header
+// header.append(message); // adds the element as the last child, when we use it here it moves the element as the last child
+
+// header.append(message.cloneNode(true)); // true: all the child elements will be copied too
+header.append(message);
+// header.before(message); // copies the element before it
+// header.after(message); // copies the element
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    console.log('halo');
+    message.remove();
+  });
