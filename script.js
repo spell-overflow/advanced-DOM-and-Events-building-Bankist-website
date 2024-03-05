6ed6343b48491c68cbfdb14c9525551e4deb50f5
Anneke Hugenberg <Annekehugenberg@googlemail.com>
2024-03-05 19:34:02 +0100

    change dot-color of slider component

--- script.js
+++ script.js
@@ -147,7 +147,6 @@ window.addEventListener('scroll', function (e) {
   if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 }); */
-
 // Sticky navigation: Intersection Observer API
 
 // const obsCallback = function (entries, observer) {
